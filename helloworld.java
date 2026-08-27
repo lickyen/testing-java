324231a80ce5f30d28b9a15266dda427643a618d
lickyen <lickyenayyappamp@gmail.com>
2026-08-27 15:22:18 +0530

    Update welcome message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("Welcome to a Mandangada family");
+		System.out.println("Welcome to a Mandangada family to all");
 	}
 }
